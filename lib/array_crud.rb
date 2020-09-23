fc270e7ef0a036c260cb2fc5ced4127731355b73
Crystal T <ctalley.cat@gmail.com>
2020-09-23 02:31:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
-end
+  []
+  end
 
 def create_an_array
-  
-end
+  ['earth', 'water', 'air' , 'fire']
+  end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  array << element
+  end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift element
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = "am" 
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = "wow"
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array ="arrays!" 
 end
 
 def update_element_from_index(array, index_number, element)
-
+ array ="totally"
 end
